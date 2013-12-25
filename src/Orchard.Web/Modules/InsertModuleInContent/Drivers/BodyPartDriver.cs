b4c65b723d52b05fa7868cdc660c556370bda6a4
dnevnik <reliagram@gmail.com>
2013-12-25 18:56:15 +0400

    insertWidgetInContent module

---
+++ src/Orchard.Web/Modules/InsertModuleInContent/Drivers/BodyPartDriver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Common.Models;
+
+namespace InsertModuleInContent.Drivers
+{
+    public class BodyPartDriver : ContentPartDriver<BodyPart> {
+
+    }
+}
