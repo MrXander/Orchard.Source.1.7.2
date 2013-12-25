e0a494463b9bd87dd1c49073afdb731da6991736
dnevnik <reliagram@gmail.com>
2013-12-25 18:56:15 +0400

    insertWidgetInContent module

---
+++ src/Orchard.Web/Modules/InsertModuleInContent/Filters/HtmlFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using Orchard.Services;
+using Orchard.Widgets.Models;
+
+namespace InsertModuleInContent.Filters
+{
+    public class HtmlFilter : IHtmlFilter {
+        private readonly IContentManager _contentManager;
+
+        public HtmlFilter(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public string ProcessContent(string text, string flavor) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var widgets =_contentManager.Query<WidgetPart, WidgetPartRecord>().Where(x => x.Name == "TestWidget").List();
+            stopwatch.Stop();
+            Debug.WriteLine(stopwatch.Elapsed.TotalSeconds);
+            var widget = widgets.FirstOrDefault();
+            if (widget != null) {
+                
+            }
+            return text;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/InsertModuleInContent/Models/WidgetParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsertModuleInContent.Models
+{
+    public class WidgetParser
+    {
+        public static void InsertWidgets() {
+            
+        }
+    }
+}
